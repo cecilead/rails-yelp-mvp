3fce860259defd6b7549e7264ee4236680ec191c
CECILE ADAM <cecileadammorvan@gmail.com>
2023-11-15 15:25:14 +0100

    add exercice but not finished

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   resources :restaurants do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: %i[new create]
   end
+  resources :reviews, only: %i[show edit update destroy]
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'PAPAPASTA',
+    address: 'NANTES',
+    phone_number: '06 07 08 09 01',
+    category: 'italian'
+  },
+  {
+    name: 'steak frites',
+    address: 'rennes',
+    phone_number: '06 07 08 09 02',
+    category: 'french'
+  },
+  {
+    name: 'whitefields',
+    address: 'cesson',
+    phone_number: '06 35 98 54 78',
+    category: 'chinese'
+  },
+  {
+    name: 'pizza',
+    address: 'paris',
+    phone_number: '02 96 87 45 25',
+    category: 'italian'
+  },
+  {
+    name: 'sushi',
+    address: 'london',
+    phone_number: '02 96 87 45 25',
+    category: 'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
